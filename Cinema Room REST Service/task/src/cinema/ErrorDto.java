20d5ea078373388eb7b06f07c1036d954541116d
NJBodam <bodambest@yahoo.com>
2022-04-30 12:31:08 +0100

    edited errorDto again

--- Cinema Room REST Service/task/src/cinema/ErrorDto.java
+++ Cinema Room REST Service/task/src/cinema/ErrorDto.java
@@ -7,4 +7,12 @@ public class ErrorDto {
         this.error = error;
     }
 
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
 }
